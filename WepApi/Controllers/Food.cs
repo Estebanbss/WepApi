5313eeef06a941043db418dc0faa41966e614b4c
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-01-24 15:08:13 -0500

    Add project files.

---
+++ WepApi/Controllers/Food.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WepApi.Controllers
+{
+    [ApiController]
+    public class Food: ControllerBase
+    {
+        [HttpGet]
+        [Route("/food")]
+        public string GetFood()
+        {
+            return "Return all the food";
+        }
+
+    }
+}
---
+++ WepApi/Controllers/Plants.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WepApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Plants : ControllerBase
+    {
+        [HttpGet]
+
+        public string GetPlants()
+        {
+            return "Reading all the plants";
+        }
+
+        [HttpGet("{id}")]
+
+        public string GetPlantById(int id)
+        {
+            return $"Reading plant {id}";
+        }
+
+        [HttpPost]
+
+        public string CreatePlant()
+        {
+            return $"Creating a plant";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdatePlant(int id)
+        {
+            return $"Updating plant: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeletePlant(int id)
+        {
+            return $"Delete plant: {id}";
+        }
+
+    }
+}
